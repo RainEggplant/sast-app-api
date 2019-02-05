26497b445dd7a05e758b3e9c2294d7d5f5e4c2a3
root <root@d82957bf3c42>
2019-02-05 11:17:28 +0000

    Removed files managed by codegen

--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t yingrui205/sast-app-api:$TRAVIS_TAG -t yingrui205/sast-app-api:latest .
-docker push yingrui205/sast-app-api:$TRAVIS_TAG
-docker push yingrui205/sast-app-api:latest
